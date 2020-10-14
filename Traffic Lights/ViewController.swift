7ea8369a30ce17594c3af327f083ee0d1a5d7bf8
ykGoose <yokogus@gmail.com>
2020-10-15 01:45:18 +0900

    some update

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var lightRedView: UIView!
+    @IBOutlet var lightYellowView: UIView!
+    @IBOutlet var lightGreenView: UIView!
+    @IBOutlet var lightSwitchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lightRedView.alpha = 0.3
+        lightRedView.layer.cornerRadius = 50
+        lightYellowView.alpha = 0.3
+        lightYellowView.layer.cornerRadius = 50
+        lightGreenView.alpha = 0.3
+        lightGreenView.layer.cornerRadius = 50
+        lightSwitchButton.layer.cornerRadius = 10
+
     }
 
-
+    @IBAction func buttonPressed() {
+        lightGreenView.alpha = 1
+        lightSwitchButton.setTitle("NEXT", for: .normal)
+    }
+    
 }
 
